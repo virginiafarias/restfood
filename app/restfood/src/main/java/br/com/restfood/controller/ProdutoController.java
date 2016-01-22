e98e25d9442a673d6cf066c43eec44bd47da6957
Virginia <virginia.farias7@gmail.com>
2016-01-22 16:55:01 -0300

    Configuração inicial do projeto

---
+++ app/restfood/src/main/java/br/com/restfood/controller/ProdutoController.java
@@ -0,0 +1,5 @@
+package br.com.restfood.controller;
+
+public class ProdutoController {
+
+}
---
+++ app/restfood/src/main/java/br/com/restfood/model/Produto.java
@@ -0,0 +1,65 @@
+package br.com.restfood.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String codigo;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	private Double preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+
+}
